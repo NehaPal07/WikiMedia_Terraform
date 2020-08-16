1359f69e4d6cf141ba2736be6f17fed73bc4c385
NehaPal07 <31745036+NehaPal07@users.noreply.github.com>
2020-08-16 23:20:46 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "aws_access_key" {
+  default = "*********************"
+}
+
+variable "aws_secret_key" {
+  default = "**************"
+}
+
+variable "key_name" {
+  default = "EC2_User"
+}
+
+variable "region" {
+   default = "us-east-2"
+ }
+
+variable "network_address_space" {
+   default = "10.1.0.0/16"
+  }
+
+variable "subnet1_address_space" {
+   default = "10.1.0.0/24"
+ }
